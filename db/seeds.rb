d2a6aa8afd4d145bab41d7b3ec6f6a26f4ad0433
Ricardo Nelumba <abstract25@icloud.com>
2023-09-13 15:54:06 +0100

    Done

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,20 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
+restaurant = [
+  {
+    name: 'Restaurant 1',
+    address: '123 Main Street',
+    category: 'Italian'
+    phone_number: '07385674839'
+  },
+  {
+    name: 'Restaurant 2',
+    address: '456 Ocean Avenue',
+    category: 'Japanese'
+  },
+]
+# Create at least 5 valid restaurant records
+
 # Examples:
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
